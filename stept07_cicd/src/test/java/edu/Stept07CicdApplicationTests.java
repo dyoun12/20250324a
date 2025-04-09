ae973db252b05f210cb1f35064ec89e1603f8aff
dyoun12 <gdaeyeon002@gmail.com>
2025-04-09 10:44:27 +0900

    test : 코드 업데이트

--- stept07_cicd/src/test/java/edu/Stept07CicdApplicationTests.java
+++ stept07_cicd/src/test/java/edu/Stept07CicdApplicationTests.java
@@ -3,11 +3,17 @@ package edu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Stept07CicdApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAddition() {
+		int result = 12 + 3;
+		System.out.println("비교전 ------");
+		assertEquals(15, result);
+		System.out.println("비교후 ------");
 	}
 
 }
